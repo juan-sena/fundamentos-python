43a83afad37aa0faf93e8b5f33c193ce044c4da9
juan-sena <juannoriegasena@gmail.com>
2026-04-22 20:33:20 -0500

    Print, falata seguir terminando

---
+++ src/print.py
@@ -0,0 +1,9 @@
+print ("¡Hola, Mundo!")
+print ("¡Hola, Juan!")
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\nay se la llevó.")
+
+print("La Witsi Witsi Araña", "subió", "a su telaraña.")
+
---
+++ src/print_y_sus_argumentos.py
@@ -0,0 +1,14 @@
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+print("Mi nombre es ", end="")
+print("Monty Python.")
+
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+print("Mi", "nombre", "es", sep="_", end="*")
+
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
